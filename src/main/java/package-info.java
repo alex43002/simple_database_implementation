1b9295b80cb220552228c7c93c70ba0605dfe1f5
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-03-22 18:54:53 -0400

    Small change to package-info

--- src/main/java/package-info.java
+++ src/main/java/package-info.java
@@ -1,6 +1,6 @@
 /**
  * <p>This package serves to run the main codebase</p>
- * <p>Dependencies found in {@link src.main.test}</p>
+ * <p>Dependencies found in {@link main.test}</p>
  * 
  * @version 0.01
  * @author Alexander DeJesus
